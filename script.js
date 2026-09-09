b8bf27a3da570c395b30aa010395d25ce25c87bd
trungdq-nia <trungdq.nia@gmail.com>
2026-09-09 10:38:56 +0700

    90%

--- script.js
+++ script.js
@@ -71,13 +71,13 @@ document.addEventListener('DOMContentLoaded', () => {
     
     projLink.addEventListener('click', (event) => {
       event.preventDefault();
-      projectFrame.src = "/projects/"+ project + ".html";
+      projectFrame.src = "/LoanProfile/projects/"+ project + ".html";
       openModal();
     });
 
     projLogo.addEventListener('click', (event) => {
       event.preventDefault();
-      projectFrame.src = "/projects/"+ project + ".html";
+      projectFrame.src = "/LoanProfile/projects/"+ project + ".html";
       openModal();
     });
   });
@@ -109,9 +109,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
-
-
-
 
 function initSpineArtwork(container) {
 
